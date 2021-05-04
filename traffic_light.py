d227006806963f051ed080ec75f1810f25bc8ac0
Bo Urskov <mail@bourskov.dk>
2021-05-04 23:09:47 +0200

    .

--- traffic_light.py
+++ traffic_light.py
@@ -13,22 +13,22 @@ GPIO.setup(YELLOW, GPIO.OUT) # PWM pin set as output
 GPIO.setup(GREEN, GPIO.OUT) # PWM pin set as output
 
 # Initial state for LEDs:
-GPIO.output(RED, GPIO.LOW)
-GPIO.output(YELLOW, GPIO.LOW)
-GPIO.output(GREEN, GPIO.LOW)
+GPIO.output(RED, GPIO.HIGH)
+GPIO.output(YELLOW, GPIO.HIGH)
+GPIO.output(GREEN, GPIO.HIGH)
 
 
 while True:
-    GPIO.output(RED,GPIO.HIGH)
+    GPIO.output(RED,GPIO.LOW)
     time.sleep(5)
-    GPIO.output(YELLOW,GPIO.HIGH)
+    GPIO.output(YELLOW,GPIO.LOW)
     time.sleep(2)
-    GPIO.output(GREEN,GPIO.HIGH)
-    GPIO.output(RED, GPIO.LOW)
-    GPIO.output(YELLOW, GPIO.LOW)
-    time.sleep(5)
-    GPIO.output(GREEN, GPIO.LOW)
+    GPIO.output(GREEN,GPIO.LOW)
+    GPIO.output(RED, GPIO.HIGH)
     GPIO.output(YELLOW, GPIO.HIGH)
-    time.sleep(1)
+    time.sleep(5)
+    GPIO.output(GREEN, GPIO.HIGH)
     GPIO.output(YELLOW, GPIO.LOW)
-    GPIO.output(RED,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(YELLOW, GPIO.HIGH)
+    GPIO.output(RED,GPIO.LOW)
